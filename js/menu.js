cc23b514ff7230e3e65b67d30177164e381f3486
exodojaffar <exodojaffar@gmail.com>
2021-04-08 17:43:01 -0400

    add: menu animation and trigger

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,14 @@
-document.querySelect("#menu")
+var menuButton = document.querySelector("#menu-icon")
+var menu = document.querySelector("#menu")
+
+menuButton.addEventListener('click',  ()=> {
+	
+	if(menu.classList.contains('hide')){
+		menu.classList.remove("hide")
+		menu.classList.add("loading")		
+	}else{
+		menu.classList.remove("loading")
+		menu.classList.add("hide")		
+	}
+	
+})
