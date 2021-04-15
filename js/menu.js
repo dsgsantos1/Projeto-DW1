ac455f7e35ffdc8df778891b696e7232cb789e65
exodojaffar <exodojaffar@gmail.com>
2021-04-15 12:00:39 -0400

    feat: change button icon when the menu is enable or not

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,29 @@
 var menuButton = document.querySelector("#menu-icon")
 var menu = document.querySelector("#menu")
 
-menuButton.addEventListener('click',  ()=> {
+const changeButtonState = () => {
+	if(menuButton.classList.contains('icon-hamburger-menu')){
+		menuButton.classList.remove("icon-hamburger-menu")
+		menuButton.classList.add("icon-close-x")		
+	}else{
+		menuButton.classList.remove("icon-close-x")
+		menuButton.classList.add("icon-hamburger-menu")		
+	}
+}
+
+const changeMenuState = () => {
 	
 	if(menu.classList.contains('hide')){
 		menu.classList.remove("hide")
 		menu.classList.add("loading")		
 	}else{
 		menu.classList.remove("loading")
-		menu.classList.add("hide")		
+		menu.classList.add("hide")
+
+		changeButtonState()		
 	}
 	
-})
+}
+
+menu.addEventListener('animationend',  changeButtonState)
+menuButton.addEventListener('click',  changeMenuState)
