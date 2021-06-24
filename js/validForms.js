cf919028508e1a817cfb8c6dc9df75c4e8e74805
exodojaffar <exodojaffar@gmail.com>
2021-06-23 21:52:56 -0400

    feat: adiciona o codigo para a funcionalidade de validação do forms

---
+++ js/validForms.js
@@ -0,0 +1,137 @@
+const form = document.querySelector('form#form')
+// const allInputs = form.querySelectorAll('input, textarea, select') 
+const allInputs = form.querySelectorAll('.input-field') 
+
+
+// Globais
+let valoresCampos = new Object();
+
+allInputs.forEach( (input) => {
+	const inputName = input.getAttribute('name')
+
+	valoresCampos = {
+		...valoresCampos,
+		[inputName]: undefined
+	}
+
+})
+
+
+// Funcs
+const validateTests = {
+	"n-null":(value) => {
+
+		if (["", undefined].includes(value)) {
+
+			return "Preenche isso ae";
+		}
+
+		return true;
+	},
+
+	"email": (value) =>{
+		const regexFormato = /(\w+.)*@((\w+\.)+|(\w+)+)\w+/g
+
+		const matchValue = value.match(regexFormato) != null ? value.match(regexFormato)[0] : "";
+
+		const valido = (matchValue.length > 0 && matchValue.length == value.length)
+
+		if (! valido) {
+			return "Email invalido"
+		}
+
+		return true;
+	},
+
+	"tel": (value) => {
+		const regexFormato = /\([0-9]*\)[ ,\-,0-9]*/g
+
+		const matchValue = value.match(regexFormato) != null ? value.match(regexFormato)[0] : "";
+		const formatoValido = (matchValue.length > 0 && matchValue.length == value.length)
+
+		const tamanhoTel = value.replace(/\([0-9]*\)/, "").replace(/[ ,-]/g, '').length
+
+		const valido = (formatoValido && ( [8,9].includes(tamanhoTel) ))
+
+		if (! valido) {
+
+			if (formatoValido) {
+				return "Telefone invalido - Tamanho invalido" 
+			}
+
+			return "Telefone invalido - Formato invalido" 
+
+		}
+
+		return true;
+	}
+}
+
+const validateField = (eventProps) => {
+	const {
+		target,
+		currentTarget
+	} = eventProps;
+
+	const inputType = target.getAttribute("type")
+	const inputName = currentTarget.getAttribute('name')
+	const attributes = currentTarget.getAttributeNames()
+
+
+
+	const validations = attributes.filter( (nameAttribute) => {return Object.keys(validateTests).slice(Object.keys(validateTests).indexOf('n-null'), 1) .includes(nameAttribute)})
+
+	let {value, checked} = target
+	let valido = true
+
+	if (inputType == "checkbox") {
+		value = checked
+	}
+
+	console.log(value)
+
+	for (funcName of validations) {
+		const result = validateTests[funcName].call(this, value)
+
+		if (result != true) {
+			valido = false
+			alert(result)
+			break;
+		}
+	}
+
+	if (valido) {
+		valoresCampos[inputName] = value
+	}else{
+		valoresCampos[inputName] = undefined
+	}
+}
+
+// Validação do Forms e Envio
+const validarForms = () => {
+	const someInputs = form.querySelectorAll('.input-field + [n-null]') 
+
+	let valeu = true
+
+	for(input of someInputs){
+		const inputName = input.getAttribute("name")
+
+		if (validateTests[inputName] == undefined) {
+			valeu = false
+			break;
+		}
+	}
+
+	if (!valeu) {
+		alert("Arruma esse forms!!")
+		return;
+
+	}else{
+		form.submit()
+	}
+}
+
+// Add Event on change 
+allInputs.forEach((input) => {
+	input.addEventListener("change", validateField)
+})
